19b1ca6f857ffb924ae2561dcf431a2b4bffd09b
Anne Halsall <anne@quora.com>
2012-06-20 12:50:47 -0700

    Initial Commit

---
+++ AKWebView/AKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AKAppDelegate.h
+//  AKWebView
+//
+//  Created by Anne Halsall on 6/20/12.
+//  Copyright (c) 2012 Quora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AKWebView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AKWebView
+//
+//  Created by Anne Halsall on 6/20/12.
+//  Copyright (c) 2012 Quora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AKAppDelegate class]));
+    }
+}
